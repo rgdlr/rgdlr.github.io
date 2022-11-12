333070ad3d71dbaa5ec0b05feee3fc54bfa159f3
rgdlr <rodrigr12@hotmail.com>
2022-11-12 15:35:18 +0100

    Update

--- javascript/projects/cookie/script.js
+++ javascript/projects/cookie/script.js
@@ -1,3 +1,18 @@
 "use strict";
 
-import {} from "../../src/js/index.js";
+import { initializeModal, MODAL } from "../../src/js/index.js";
+
+const modal = {
+  accept: window.document.querySelector(MODAL.SELECTOR.ACCEPT),
+  body: window.document.querySelector(MODAL.SELECTOR.BODY),
+  cancel: window.document.querySelector(MODAL.SELECTOR.CANCEL),
+  close: window.document.querySelector(MODAL.SELECTOR.CLOSE),
+  footer: window.document.querySelector(MODAL.SELECTOR.FOOTER),
+  header: window.document.querySelector(MODAL.SELECTOR.HEADER),
+  modal: window.document.querySelector(MODAL.SELECTOR.MODAL),
+  open: window.document.querySelector(MODAL.SELECTOR.OPEN),
+  title: window.document.querySelector(MODAL.SELECTOR.TITLE),
+  window: window.document.querySelector(MODAL.SELECTOR.WINDOW),
+};
+
+initializeModal(modal);
