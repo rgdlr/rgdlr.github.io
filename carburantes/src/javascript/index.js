729eb4ba5bf5c14a21ba0d7279cdec483a41fb43
rgdlr <rodrigr12@hotmail.com>
2023-02-15 02:06:39 +0100

    Update

---
+++ carburantes/src/javascript/index.js
@@ -0,0 +1,11 @@
+AOS.init({ startEvent: 'DOMContentLoaded', once: true, offset: 150, delay: 150 });
+
+// function cropSvg(svgEl) {
+//   const bbox = svgEl.getBBox();
+//   svgEl.setAttribute('viewBox', `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`);
+// }
+
+// document.querySelectorAll('svg').forEach((svg) => cropSvg(svg));
+
+// const carousel = document.querySelector('#carouselExampleCaptions');
+// carousel.addEventListener('click', (_event) => carousel.requestFullscreen());
