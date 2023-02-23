d24c7bab09cc711875a4d6499cbe94836ea85510
rgdlr <rodrigr12@hotmail.com>
2023-02-24 00:43:31 +0100

    Update

--- casa-rural/v2/src/javascript/index.js
+++ casa-rural/v2/src/javascript/index.js
@@ -1,4 +1,4 @@
-AOS.init({ startEvent: 'DOMContentLoaded', once: true });
+AOS.init({ startEvent: 'DOMContentLoaded', once: true, offset: 250, delay: 250 });
 
 function cropSvg(svgEl) {
   const bbox = svgEl.getBBox();
@@ -7,5 +7,29 @@ function cropSvg(svgEl) {
 
 document.querySelectorAll('svg').forEach((svg) => cropSvg(svg));
 
-// const carousel = document.querySelector('#carouselExampleCaptions');
+// const carousel = document.querySelector('#rooms-carousel');
 // carousel.addEventListener('click', (_event) => carousel.requestFullscreen());
+
+function highlightCarouselText(params) {
+  const carouselItems = document.querySelectorAll('.carousel-item');
+
+  const observer = new MutationObserver((mutation) => {
+    mutation.forEach((entry) => {
+      const dataCarouselText = document.querySelector(`[data-carousel-text="aire-acondicionado"]`);
+      dataCarouselText.style.color = '';
+    });
+    mutation.forEach((entry) => {
+      const dataCarouselImage = entry.target.getAttribute('data-carousel-image');
+      const dataCarouselText = document.querySelector(
+        `[data-carousel-text="${dataCarouselImage}"]`
+      );
+      if (entry.target.classList.contains('active')) {
+        dataCarouselText.style.color = '#F00';
+      }
+    });
+  });
+
+  carouselItems.forEach((carouselItem) => {
+    observer.observe(carouselItem, { attributes: true });
+  });
+}
