307cd10679703853c846e8142e1a6c8d737ad406
rgdlr <rodrigr12@hotmail.com>
2023-03-20 00:42:26 +0100

    Update

--- css/src/pages/carousel/script.js
+++ css/src/pages/carousel/script.js
@@ -1,9 +1,16 @@
+// TODO : use IntersectionObserver to toggle slide--active
+
 const allPrevious = document.querySelectorAll(".carousel-previous");
 allPrevious.forEach((previous) =>
   previous.addEventListener("click", (event) => {
     const carousel = previous.closest(".carousel");
     const slides = carousel.querySelector(".slides");
     const slideActive = slides.querySelector(".slide.slide--active");
+    if (!(slideActive && slideActive.previousElementSibling)) {
+      return;
+    }
+    slideActive.classList.remove("slide--active");
+    slideActive.previousElementSibling.classList.add("slide--active");
     slides.scrollBy({ behavior: "smooth", left: -1, top: -1 });
   })
 );
@@ -14,6 +21,11 @@ allNext.forEach((next) =>
     const carousel = next.closest(".carousel");
     const slides = carousel.querySelector(".slides");
     const slideActive = slides.querySelector(".slide.slide--active");
+    if (!(slideActive && slideActive.nextElementSibling)) {
+      return;
+    }
+    slideActive.classList.remove("slide--active");
+    slideActive.nextElementSibling.classList.add("slide--active");
     slides.scrollBy({ behavior: "smooth", left: 1, top: 1 });
   })
 );
